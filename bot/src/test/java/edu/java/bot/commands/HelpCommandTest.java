ca849448751baaf595d8a727a633d26f52edbf92
EvsArt <x.evseev@yandex.ru>
2024-02-18 21:22:38 +0300

    change token handling to env variable

---
+++ bot/src/test/java/edu/java/bot/commands/HelpCommandTest.java
@@ -0,0 +1,20 @@
+package edu.java.bot.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelpCommandTest {
+
+    @Test
+    void isTrigger() {
+    }
+
+    @Test
+    void getHelpMessage() {
+    }
+
+    @Test
+    void handle() {
+    }
+}
