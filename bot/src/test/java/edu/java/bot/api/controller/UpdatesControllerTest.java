610686ae9bd18a10329eaf2e740dddcb4e4a73f6
EvsArt <x.evseev@yandex.ru>
2024-03-04 16:32:28 +0300

    add updatesControllerTest and remove logback from kafka-test

---
+++ bot/src/test/java/edu/java/bot/api/controller/UpdatesControllerTest.java
@@ -0,0 +1,11 @@
+package edu.java.bot.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UpdatesControllerTest {
+
+    @Test
+    void postUpdate() {
+    }
+}
---
+++ scrapper/src/test/java/edu/java/api/controller/ChatControllerTest.java
@@ -0,0 +1,11 @@
+package edu.java.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ChatControllerTest {
+
+    @Test
+    void registerChat() {
+    }
+}
